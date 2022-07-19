aacc4b741fd59c30e767986e5bb5b777b0c0e11e
Simone Mauro <simon.mauro@virgilio.it>
2022-07-19 14:11:45 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,47 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index(){
+            List<Pizza> pizze = new PizzaContext().Pizzas.ToList();
+            ViewData["title"] = "Menù pizze";
+            return View(pizze);
+        }
+
+        public IActionResult Show(int id){
+            Pizza pizzaResult = new PizzaContext().Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+            if (pizzaResult == null){
+                return NotFound($"Non esiste nessuna pizza con l'id {id}");
+            }
+            else{
+                return View(pizzaResult);
+
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create(){
+            return View("Create");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza){
+            if (!ModelState.IsValid){
+                return View("Create", pizza);
+            }
+
+            using(PizzaContext context = new PizzaContext()){
+                context.Pizzas.Add(pizza);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+    }
+}
